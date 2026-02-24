818660bba24ca8d16a90812c7f8c9dda802aa63d
nagul2 <hayani0000@gmail.com>
2026-02-24 15:20:51 +0900

    backBehavior까지

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,8 +1,12 @@
-import { useRouter } from "expo-router";
+import { usePathname, useRouter } from "expo-router";
 import { Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   const router = useRouter();
+  const pathname = usePathname();
+
+  console.log("pathname", pathname);
+
   return (
     <View
       style={{
@@ -12,10 +16,14 @@ export default function Index() {
       }}
     >
       <TouchableOpacity onPress={() => router.push(`/`)}>
-        <Text>For you</Text>
+        <Text style={{ color: pathname === "/" ? "red" : "black" }}>
+          For you
+        </Text>
       </TouchableOpacity>
       <TouchableOpacity onPress={() => router.push(`/following`)}>
-        <Text>Following</Text>
+        <Text style={{ color: pathname === "/" ? "black" : "red" }}>
+          Following
+        </Text>
       </TouchableOpacity>
       <TouchableOpacity onPress={() => router.push(`/@user/post/1`)}>
         <Text>게시글1</Text>
